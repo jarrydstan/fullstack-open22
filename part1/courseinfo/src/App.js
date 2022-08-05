6ad0ea2f7f896234a702e37363cd2dcc7d40d538
jarrydstan <jarrydstanbrook@hotmail.com>
2022-08-05 17:46:18 +1000

    refactor

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -5,7 +5,7 @@ const Header = (props) => {
     </div>
   );
 };
-const Content = (props) => {
+const Part = (props) => {
   return (
     <div>
       <p>
@@ -20,11 +20,21 @@ const Content = (props) => {
     </div>
   );
 };
+const Content = (props) => {
+  return (
+    <div>
+      <Part part1={props.part1} exercises1={props.exercises1}/>
+      <Part part2={props.part2} exercises2={props.exercises2}/>
+      <Part part3={props.part3} exercises3={props.exercises3}/>
+    </div>
+  );
+};
 const Total = (props) => {
   return (
     <div>
       <p>
-        Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+        Number of exercises{" "}
+        {props.exercises1 + props.exercises2 + props.exercises3}
       </p>
     </div>
   );
